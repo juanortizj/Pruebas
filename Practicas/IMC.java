a7ed26b9fc10dd5c04a56165c05ab1e7ab881f83
juanortizj <juan.ortiziz@campusucc.edu.co>
2025-05-08 16:12:45 +0000

    mejoras3

--- Practicas/IMC.java
+++ Practicas/IMC.java
@@ -10,19 +10,30 @@ public class IMC {
         System.out.println("Por favor ingrese su peso: ");
         double peso = entrada.nextDouble();
 
-        System.out.println("Porfavor ingrese su altura en metros: ");
+        System.out.println("Por favor ingrese su altura en metros: ");
         double altura = entrada.nextDouble();
 
-        double imc peso / (altura * altura);
+        double imc = peso / (altura * altura);
         System.out.println("Su IMC es:" + imc);
 
 
         if (imc < 18.5) {
-            System.out.println(" Segun la tabla del IMC (Indice de Masa Corporal) ussted tiene bajo peso. ");
+            System.out.println(" Segun la tabla del IMC (Indice de Masa Corporal) usted tiene bajo peso. ");
         }
 
-        if (imc >= 18.6 && imc <= 24.9) {
-            System.out.println("Segun la tabla")
+        else if (imc >= 18.5 && imc <= 24.9) {
+            System.out.println("Segun la tabla del IMC (Indice de Masa Corporal), Usted tiene un peso normal.");
         }
+
+        else if (imc >= 25 && imc < 30) {
+            System.out.println("Segun la tabla de IMC (Indice de Masa Corporal), Usted tiene sobrepeso. ");            
+        }
+
+        else {
+            System.out.println("Segun la tabla del IMC (Indice de Masa Corporal), usted tiene obesidad");
+        }
+
+        entrada.close();
+        System.out.println("Gracias por usar el programa de IMC (Indice de Masa Corporal).");
     }
 }
